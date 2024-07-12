1a541fdc56e5be8f12878f8c36a2d90f6e285f4b
matthewdo1231 <matthewdoarda@gmail.com>
2024-07-13 00:26:19 +0800

    dashboard and login

---
+++ header.php
@@ -0,0 +1,7 @@
+   <nav class="nav-bar" >
+      <div class="nav-link-logo">Diversee</div>
+      <div class="nav-links"></div>
+      <div class="nav-links-authentication">
+        <a>Login</a>
+      </div>    
+    </nav>
